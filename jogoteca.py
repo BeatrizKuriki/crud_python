f6ee7c398eceae8f7fccb2f3a9cc23dbc45c3794
Beatriz Kuriki <b.f.kuriki@gmail.com>
2023-04-11 17:18:30 -0300

    template html e classe jogo

--- jogoteca.py
+++ jogoteca.py
@@ -1,9 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
+class Jogo:
+    def __init__(self, nome, categoria, console):
+        self.nome=nome
+        self.categoria=categoria
+        self.console=console
+
 
 app = Flask(__name__)
 
 @app.route('/inicio')
 def ola():
-    return '<h1>Olá, Mundo!!</h1>'
+    jogo1=Jogo('TETRIS', 'Puzzle','Atari')
+    jogo2=Jogo('GOD OF WAR', 'Rack n Slash', 'PS2')
+    lista = [jogo1, jogo2]
+    return render_template('lista.html', titulo='Jogos', jogos=lista)
 
-app.run()
+app.run(host='localhost', port=8080)
