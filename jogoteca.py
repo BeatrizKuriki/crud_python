3f7938a49b23f3f8f4ec4c4d518bd04dc0e29374
Beatriz Kuriki <b.f.kuriki@gmail.com>
2023-04-12 16:55:38 -0300

    criada nova rota e template de formulário para inserção de novo jogo

--- jogoteca.py
+++ jogoteca.py
@@ -12,7 +12,12 @@ app = Flask(__name__)
 def ola():
     jogo1=Jogo('TETRIS', 'Puzzle','Atari')
     jogo2=Jogo('GOD OF WAR', 'Rack n Slash', 'PS2')
-    lista = [jogo1, jogo2]
+    jogo3=Jogo('Mortal Kombat', 'Luta', 'PS2')
+    lista = [jogo1, jogo2, jogo3]
     return render_template('lista.html', titulo='Jogos', jogos=lista)
+
+@app.route('/novo')
+def novo():
+    return render_template('novo.html', titulo='Novo Jogo')
 
 app.run(host='localhost', port=8080)
